425d411b3e9cecce3312b8ff4bd303b3aa6a1e7a
Nichole <KisbeyNichole@gmail.com>
2019-09-20 19:35:43 +0000

    added functions

--- partner-1.js
+++ partner-1.js
@@ -1,1 +1,10 @@
 //Name: Nichole
+const circumfrence = (num1) => {
+    return 2*Math.PI*num1;
+}
+const areaTriange = (num1,num2) => {
+    return 1/2*num1*num2;
+}
+const areaTrap = (num1,num2,num3) => {
+    return 1/2*num3*(num1+num2);
+}
