4d3b635342dc5f7f965e2e838fc0f9bd925517f4
Nichole <KisbeyNichole@gmail.com>
2019-09-20 19:39:47 +0000

    added functions

--- partner-1.js
+++ partner-1.js
@@ -8,3 +8,6 @@ const areaTriange = (num1,num2) => {
 const areaTrap = (num1,num2,num3) => {
     return 1/2*num3*(num1+num2);
 }
+const areaRegPoly = (num1,num2,num3) => {
+    return num3*(1/2*num1*num2);
+}
